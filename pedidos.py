6ace19d49891415397512ffc3960d5b70139edb3
calviptur <documentacao@calviptur.com.br>
2025-10-30 10:19:50 -0300

    Update pedidos.py

--- pedidos.py
+++ pedidos.py
@@ -1,4 +1,4 @@
-# app.py
+# Sistema_Pedidosv12.py
 import os
 import shutil
 import logging
@@ -1050,3 +1050,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "").lower() in {"1", "true", "yes"}
     app.run(host="0.0.0.0", port=port, debug=debug)
+
